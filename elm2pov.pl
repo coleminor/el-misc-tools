e6a93dfcca5af7eb01b3381db891a72a11415402
Cole Minor <c.minor@inbox.com>
2014-05-08 01:52:18 -0400

    fix typo

--- elm2pov.pl
+++ elm2pov.pl
@@ -390,7 +390,7 @@ sub emit_intro {
   my $sx = $map->{tile_length} * 0.5;
   my $sy = $map->{tile_breadth} * 0.5;
   my $cx = $sx / 2;
-  my $cy = $sx / 2 + 0.5;
+  my $cy = $sy / 2 + 0.5;
   my $c = vec_pov $cx, $cy, 100;
   my $l = vec_pov $cx, $cy, 0;
   my $a = "<1,1,1>";
